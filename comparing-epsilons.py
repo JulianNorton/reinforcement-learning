dce2c77990c795aa6d782a71682d94496d15a3a3
Julian Norton <julian@juliannorton.com>
2017-07-30 12:58:04 -0400

    # https://deeplearningcourses.com/c/artificial-intelligence-reinforcement-learning-in-python

--- comparing-epsilons.py
+++ comparing-epsilons.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Bandit:
 	def __init__(self, m):
 		self.m = m
@@ -14,42 +15,45 @@ class Bandit:
 		self.N += 1
 		self.mean = (1 - 1.0/self.N)*self.mean + 1.0/self.N*x
 
-	def run_experiment(m1, m2, m3, eps, N):
-		bandits = [Bandit(m1), Bandit(m2), Bandit(m3)]
 
-		data = np.empty(N)
+def run_experiment(m1, m2, m3, eps, N):
+	bandits = [Bandit(m1), Bandit(m2), Bandit(m3)]
 
-		for i in range(N):
-			p = np.random.random()
-			if p < eps:
-				j = np.random.choice(3)
-			else:
-				j = np.argmax([b.mean for b in bandits])
-			x = bandits[j].pull()
-			bandits[j].update(x)
+	data = np.empty(N)
+	
+	for i in range(N):
+		# epsilon greedy
+		p = np.random.random()
+		if p < eps:
+			j = np.random.choice(3)
+		else:
+			j = np.argmax([b.mean for b in bandits])
+		x = bandits[j].pull()
+		bandits[j].update(x)
 
-			data[i] = x
+		# for the plot
+		data[i] = x
+	cumulative_average = np.cumsum(data) / (np.arange(N) + 1)
 
-		cumulative_average = np.cumsum(data) / (np.arange(N) + 1)
+	# plot moving average ctr
+	plt.plot(cumulative_average)
+	plt.plot(np.ones(N)*m1)
+	plt.plot(np.ones(N)*m2)
+	plt.plot(np.ones(N)*m3)
+	plt.xscale('log')
+	plt.show()
 
-		plt.plot(cumulative_average)
-		plt.plot(np.ones(N)*m1)
-		plt.plot(np.ones(N)*m2)
-		plt.plot(np.ones(N)*m3)
-		plt.xscale('log')
-		plt.show()
+	for b in bandits:
+		print(b.mean)
 
-		for b in bandits:
-			print(b.mean)
-
-		return cumulative_average
-
+	return cumulative_average
 
 if __name__ == '__main__':
 	c_1 = run_experiment(1.0, 2.0, 3.0, 0.1, 100000)
 	c_05 = run_experiment(1.0, 2.0, 3.0, 0.05, 100000)
 	c_01 = run_experiment(1.0, 2.0, 3.0, 0.01, 100000)
 
+	# log scale plot
 	plt.plot(c_1, label='eps = 0.1')
 	plt.plot(c_05, label='eps = 0.05')
 	plt.plot(c_01, label='eps = 0.01')
@@ -58,6 +62,7 @@ if __name__ == '__main__':
 	plt.show()
 
 
+	# linear plot
 	plt.plot(c_1, label='eps = 0.1')
 	plt.plot(c_05, label='eps = 0.05')
 	plt.plot(c_01, label='eps = 0.01')
